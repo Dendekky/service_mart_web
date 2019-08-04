8afd71a6d3ae16407c03e9cb8fe022d20ff829d0
Elias Akande <akandetoluwalase@gmail.com>
2019-08-04 20:15:13 +0100

    corrections in review effected

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports={
-    "extends" : "airbnb-base"
+    "parser": "babel-eslint",
+    "env":{
+        "browser":true, 
+        "es6":true, 
+        "commonjs":true, 
+        "node":true
+},
+    "plugins": ["react"],
+    "extends" : ["airbnb-base", "plugin:react/recommended"]
    }
+   
