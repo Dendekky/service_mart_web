c6d1a013b278ded5051e23c81bbdc0d7c26b1bd7
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-08-09 14:17:15 +0100

    setup-sass/scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
 const App = () => <div>Hello React,Webpack 4 & Babel 7!</div>;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
@@ -18,6 +24,13 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
     ],
   },
   plugins: [
