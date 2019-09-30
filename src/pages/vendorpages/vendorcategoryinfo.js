8eb3a3512cef354737beb076ee56560f09ca8ea8
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-09-30 13:16:23 +0100

    users can paginate list of vendors

--- src/pages/vendorpages/vendorcategoryinfo.js
+++ src/pages/vendorpages/vendorcategoryinfo.js
@@ -7,7 +7,15 @@ class CategoryInfo extends React.Component {
     super(props);
     this.state = {
       category: [],
+      currentPage: 1,
+      vendorsPerPage: 3,
     };
+  }
+
+  handleClick=(event) => {
+    this.setState({
+      currentPage: Number(event.target.id),
+    });
   }
 
   componentDidMount() {
@@ -18,13 +26,41 @@ class CategoryInfo extends React.Component {
   }
 
   render() {
-    const { category } = this.state;
+    const { category, currentPage, vendorsPerPage } = this.state;
+
+    // Logic for displaying current Vendors
+    const indexOfLastVendor = currentPage * vendorsPerPage;
+    const indexOfFirstVendor = indexOfLastVendor - vendorsPerPage;
+    const currentVendors = category.slice(indexOfFirstVendor, indexOfLastVendor);
+
+    const renderVendors = currentVendors.map(vendorcategory => <div key={vendorcategory.id}>
+      <Link to={`/vendorlist/${vendorcategory.id}`} className='link'>{vendorcategory.agency_name}</Link>
+        <p>{vendorcategory.job_specification}</p>
+      </div>);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(category.length / vendorsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => <li
+          key={number}
+          id={number}
+          onClick={this.handleClick}
+        >
+          {number}
+        </li>);
+
+
     return (
         <div className="category_list">
-        {category.map(vendorcategory => <div key={vendorcategory.id}>
-          <Link to={`/vendorlist/${vendorcategory.id}`} className='link'>{vendorcategory.agency_name}</Link>
-            <p>{vendorcategory.job_specification}</p>
-          </div>)}
+        <ul>
+        {renderVendors}
+        </ul>
+        <ul className="page_numbers">
+              {renderPageNumbers}
+            </ul>
       </div>
     );
   }
